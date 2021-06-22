41993ee78c5d645933391138ed6af69a5c2af359
Fernanda Ribeiro <fer.maiafr@gmail.com>
2021-06-22 12:13:24 -0300

    Testes unitários

---
+++ src/test/java/one/digitalinnovation/personapi/service/PersonServiceTest.java
@@ -0,0 +1,51 @@
+package one.digitalinnovation.personapi.service;
+
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.utils.PersonUtils;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.Mock;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static one.digitalinnovation.personapi.utils.PersonUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personRepository.save(any(Person.class)))
+                .thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedMessage = createExpectedResponse(expectedSavedPerson.getId());
+        MessageResponseDTO successMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedMessage, successMessage);
+
+    }
+
+    private MessageResponseDTO createExpectedResponse(Long id) {
+        return MessageResponseDTO.builder()
+                .message("Updated Person with ID " + id)
+                .build();
+    }
+}
